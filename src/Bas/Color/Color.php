f18fa97a27bddca332fae23df367eb9bbcbae234
Bas Milius <basmilius@hotmail.com>
2018-01-10 08:48:42 +0100

    Added instance creators.

--- src/Bas/Color/Color.php
+++ src/Bas/Color/Color.php
@@ -88,4 +88,55 @@ class Color
 		return [$this->r, $this->g, $this->b, $this->a];
 	}
 
+	/**
+	 * Creates a new instance of {@see Color} from HSL values.
+	 *
+	 * @param float $h
+	 * @param float $s
+	 * @param float $l
+	 *
+	 * @return Color
+	 * @author Bas Milius <bas@ideemedia.nl>
+	 * @since 1.1.0
+	 */
+	public static function fromHsl (float $h, float $s, float $l): self
+	{
+		[$r, $g, $b] = ColorUtil::hslToRgb($h, $s, $l);
+
+		return new self($r, $g, $b);
+	}
+
+	/**
+	 * Creates a new instance of {@see Color} from RGB values.
+	 *
+	 * @param int $r
+	 * @param int $g
+	 * @param int $b
+	 *
+	 * @return Color
+	 * @author Bas Milius <bas@ideemedia.nl>
+	 * @since 1.1.0
+	 */
+	public static function fromRgb (int $r, int $g, int $b): self
+	{
+		return new self($r, $g, $b);
+	}
+
+	/**
+	 * Creates a new instance of {@see Color} from RGBA values.
+	 *
+	 * @param int   $r
+	 * @param int   $g
+	 * @param int   $b
+	 * @param float $a
+	 *
+	 * @return Color
+	 * @author Bas Milius <bas@ideemedia.nl>
+	 * @since 1.1.0
+	 */
+	public static function fromRgba (int $r, int $g, int $b, float $a): self
+	{
+		return new self($r, $g, $b, $a);
+	}
+
 }
