d7667a52756bca3e3b534ef84a01b3a5bc590afb
Bas Milius <basmilius@hotmail.com>
2018-12-12 00:35:58 +0100

    Revert charset.

--- src/Columba/Database/MySQLDatabaseDriver.php
+++ src/Columba/Database/MySQLDatabaseDriver.php
@@ -30,16 +30,15 @@ final class MySQLDatabaseDriver extends DatabaseDriver
 	 * @param int    $port
 	 * @param string $username
 	 * @param string $password
-	 * @param string $charset
 	 * @param array  $options
 	 * @param bool   $connectAutomatically
 	 *
 	 * @author Bas Milius <bas@mili.us>
 	 * @since 1.0.0
 	 */
-	public function __construct(string $database, string $host, int $port = 3306, $username = '', $password = '', string $charset = 'utf8mb4', array $options = [], $connectAutomatically = true)
+	public function __construct(string $database, string $host, int $port = 3306, $username = '', $password = '', array $options = [], $connectAutomatically = true)
 	{
-		$dsn = "mysql:dbname=$database;host=$host;port=$port;charset=$charset";
+		$dsn = "mysql:dbname=$database;host=$host;port=$port";
 
 		parent::__construct($dsn, $username, $password, $options, $connectAutomatically);
 	}
