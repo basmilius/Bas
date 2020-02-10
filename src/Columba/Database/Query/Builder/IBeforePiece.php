a509f8417eaacfb46f0e82e5063a2c8339fd8596
Bas Milius <basmilius@hotmail.com>
2020-02-10 22:37:08 +0100

    Copyright update

--- src/Columba/Database/Query/Builder/IBeforePiece.php
+++ src/Columba/Database/Query/Builder/IBeforePiece.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright (c) 2017 - 2020 - Bas Milius <bas@mili.us>
+ * Copyright (c) 2019 - 2020 - Bas Milius <bas@mili.us>
  *
- * This file is part of the Latte Framework package.
+ * This file is part of the Columba package.
  *
  * For the full copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
