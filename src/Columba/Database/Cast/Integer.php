a74ce2fdf516b8ec7eb0de543af3d51fd146e199
Bas Milius <basmilius@hotmail.com>
2020-08-21 22:40:13 +0200

    feat(database): Adds Integer cast.

---
+++ src/Columba/Database/Cast/Integer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) 2017 - 2020 - Bas Milius <bas@mili.us>
+ *
+ * This file is part of the Latte Framework package.
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Columba\Database\Cast;
+
+use Columba\Database\Model\Model;
+
+/**
+ * Class Integer
+ *
+ * @author Bas Milius <bas@mili.us>
+ * @package Columba\Database\Cast
+ * @since 1.6.0
+ */
+class Integer implements ICast
+{
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	public function get(Model $model, string $key, $value, array $data)
+	{
+		return intval($value);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	public function set(Model $model, string $key, $value, array $data)
+	{
+		return $value;
+	}
+
+}
